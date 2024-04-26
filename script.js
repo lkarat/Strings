074aa9c4e038823d7b888ae9e2a891495b73e806
Luiz Karat <luiz.karat.silva@gmail.com>
2024-04-26 09:09:32 -0300

    aprendendo

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Aula de Strings")
+
+
+console.log("pelé".toUpperCase());
+console.log("PELÉ".toLowerCase());
+console.log("Gosto de comer geléia".includes("comer"));// "includes" busca palavras na frase. É case sensitive
+console.log("Gosto de comer geléia".includes("comer", "banana"));
+console.log("Gosto de comer geléia".includes("comer" && "banana")); //"includes" admite operadores lógicos
