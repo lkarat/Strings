bd8334107f80cff1621af60e53fe9ba568b4f254
Luiz Karat <luiz.karat.silva@gmail.com>
2024-04-28 21:44:21 -0300

    Exercício 1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Aula de Strings")
+/*qconsole.log("Aula de Strings")
 
 
 console.log("pelé".toUpperCase());
@@ -23,5 +23,23 @@ let novaFrase = frase.replace("maçãs", "laranjas");
 console.log(novaFrase);
 
 let novoTexto = texto.replace("bilolinha","jornal")
-console.log(novoTexto);
+console.log(novoTexto);*/
 
+//Exercícios
+// Exercício 1 Declare uma função 'imprimirPessoa' e crie um programa que peça ao usuário para inserir seu nome, e sua cor favorita. Em seguida, o programa deve imprimir a mensagem: "A cor favorita de pessoa é cor." Faça o programa duas vezes! Na primeira, use concatenação, e na segunda, Template Strings.
+
+function imprimirPessoa1(){
+    let nome1=prompt("insira seu nome:")
+    let corFavorita1=prompt("Insira sua cor favorita")
+    console.log("A cor favorita de "+nome1+" é "+corFavorita1+".")
+    
+}
+imprimirPessoa1()
+
+function imprimirPessoa2(){
+    let nome2=prompt("insira seu nome:")
+    let corFavorita2=prompt("Insira sua cor favorita")
+    console.log(`A cor favorita de ${nome2} é ${corFavorita2}.`)
+    
+}
+imprimirPessoa2()
