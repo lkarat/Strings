c6eda716f7524a96f86330133c7bea7ecd326186
Luiz Karat <luiz.karat.silva@gmail.com>
2024-04-28 21:58:49 -0300

    exercício 3

--- exercicios.js
+++ exercicios.js
@@ -17,3 +17,20 @@ function imprimirPessoa2(){
 }
 imprimirPessoa2()*/
 
+
+
+
+
+//Exercício 2 Altere a forma com que o texto aparece. Altere a primeira versão do programa para que a pessoa também envie sua citação favorita. Essa citação deve ser impressa entre aspas, após a frase inicial. Altere a segunda versão do programa para que a string seja impressa dessa forma (utilizando apenas um console.log): Nome: fulano Cor Favorita: cor citacao: "bla bla bla"
+
+//Exercício 3 - Faça com que o nome da pessoa sempre seja exibido em letras maiúsculas - Exiba no console quantos caracteres tem o nome da pessoa - Verifique se o nome da pessoa possui a letra A
+
+function imprimirPessoa3(){
+    let nome=prompt("Diga o seu nome:")
+    let tamanhoNome=nome.length
+    let nomeTemA=nome.includes("a")
+    console.log(`Seu nome é ${nome}`);
+    console.log(`Seu nome tem ${tamanhoNome} letras`);
+    console.log(`Seu nome tem a letra "a":`, nomeTemA)
+}
+imprimirPessoa3()
