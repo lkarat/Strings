ca385b9884add8b3419430a0099a878e36fd5b9d
Luiz Karat <luiz.karat.silva@gmail.com>
2024-04-28 21:15:07 -0300

    estudando

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ console.log("PELÉ".toLowerCase());
 console.log("Gosto de comer geléia".includes("comer"));// "includes" busca palavras na frase. É case sensitive
 console.log("Gosto de comer geléia".includes("comer", "banana"));
 console.log("Gosto de comer geléia".includes("comer" && "banana")); //"includes" admite operadores lógicos
+const name="Tifany"
+console.log(`A ${name} é legal`)
+
+const texto="bilolinha de jesus"
+let tamanho=texto.length
+console.log(tamanho)
+
+console.log(texto.toUpperCase());
+
+
+let frase = "Eu gosto de maçãs.";
+
+let novaFrase = frase.replace("maçãs", "laranjas");
+
+console.log(novaFrase);
+
+let novoTexto = texto.replace("bilolinha","jornal")
+console.log(novoTexto);
+
